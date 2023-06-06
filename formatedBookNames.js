db3100f3a2f20adc37b41bf44430c9dd2edb9dc3
Pedro Henrique A. Silva <pedro.henrique.a.s66@gmail.com>
2023-06-06 15:12:15 -0300

    Requisito 1 concluido, implementação função formatedBookNames

--- formatedBookNames.js
+++ formatedBookNames.js
@@ -1,7 +1,10 @@
-// const { books } = require('./data/library');
+const { books } = require('./data/library');
 
 const formatedBookNames = () => {
   // escreva seu código aqui
+  return books.map((book) => `${book.name} - ${book.genre} - ${book.author.name}`)
 };
+
+console.log(formatedBookNames())
 
 module.exports = { formatedBookNames };
