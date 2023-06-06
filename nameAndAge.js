14c5ae59adcfbc7ee51a7be5656dd812a88bb844
Pedro Henrique A. Silva <pedro.henrique.a.s66@gmail.com>
2023-06-06 15:47:05 -0300

    Corrigindo lint

--- nameAndAge.js
+++ nameAndAge.js
@@ -2,9 +2,8 @@ const { books } = require('./data/library');
 
 const nameAndAge = () => {
   const authorAge = books.map((book) => (
-    {author: book.author.name, age: (book.releaseYear - book.author.birthYear)})
-  );
-  
+    { author: book.author.name, age: (book.releaseYear - book.author.birthYear) }));
+
   return authorAge.sort((a, b) => a.age - b.age);
 };
 
